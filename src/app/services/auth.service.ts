f794ec83c8a2dd0d446c8f4eabfd930a81d0e1b6
JulianPetersen <julianpetersen92@gmail.com>
2022-03-21 16:38:38 -0300

    deploy

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,10 +11,10 @@ export class AuthService {
 
   
   //produccion
-  //private URL = 'https://api.sorteandorest.com.ar/api/'
+  private URL = 'https://api.sorteandorest.com.ar/api/'
   
   //desarrollo
-  private URL = 'http://localhost:3000/api/'
+  //private URL = 'http://localhost:3000/api/'
   constructor(private http: HttpClient, private router: Router) { }
   
 
